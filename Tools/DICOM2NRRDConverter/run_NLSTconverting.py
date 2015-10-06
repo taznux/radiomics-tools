d426b29d35c15824e993689b37b86eab4bdd5e8e
Wookjin Choi <taznux@gmail.com>
2015-10-06 10:31:31 -0400

    DICOMReadAndWrite is changed to DICOM2NRRDConverter DICOM2NRRDConverter support PET and removed PETCTConverter

--- Tools/DICOM2NRRDConverter/run_NLSTconverting.py
+++ Tools/DICOM2NRRDConverter/run_NLSTconverting.py
@@ -8,6 +8,6 @@ print data_list
 
 for data in data_list[2:]:
     print data
-    os.system('./DICOMReadAndWriteNLST.py ../DATA/NLST/'+data+' ../DATA/Patients/NLST_nrrd/'+data)
+    os.system('./DICOM2NRRDConverterNLST.py ../DATA/NLST/'+data+' ../DATA/Patients/NLST_nrrd/'+data)
 
 
