9089d44fdfb1347b4bb635c45a9a43a93e0191d4
Wookjin Choi <taznux@gmail.com>
2015-10-09 11:00:24 -0400

    Recursive Converting and add typename for linux compile

--- Tools/DICOM2NRRDConverter/DICOM2NRRDConverter.py
+++ Tools/DICOM2NRRDConverter/DICOM2NRRDConverter.py
@@ -3,18 +3,25 @@
 import os
 import sys
 
-#print 'Number of arguments:', len(sys.argv), 'arguments.'
-#print 'Argument List:', str(sys.argv)
 
-if len(sys.argv) < 3:
-    print sys.argv[0], ' [input dir] [output dir]'
-    sys.exit(-1)
-
-input_list = os.listdir(sys.argv[1])
-
-for input in input_list:
-    cmd = './BIN/DICOM2NRRDConverter '+sys.argv[1]+'/'+input+' '+sys.argv[2]
+def recur_dir(input_dir,output_dir):
+  input_list = os.listdir(input_dir)
+  nodir = True;
+  for input in input_list:
+    if os.path.isdir(input) == True:
+      recur_dir(input)
+      nodir = False
+  if nodir == True:
+    cmd = 'DICOM2NRRDConverter '+input_dir+' '+output_dir
     print cmd
     os.system(cmd)
 
 
+#print 'Number of arguments:', len(sys.argv), 'arguments.'
+#print 'Argument List:', str(sys.argv)
+
+if len(sys.argv) < 3:
+  print sys.argv[0], ' [input dir] [output dir]'
+  sys.exit(-1)
+
+  recur_dir(sys.argv[1],sys.argv[2])
