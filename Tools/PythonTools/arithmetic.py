8c2e8f4122b558c2a174a725f042707221d67f45
Wookjin Choi <choiw@mskcc.org>
2016-08-15 22:24:47 -0400

    PythonTools : ENH - support module style

--- Tools/PythonTools/arithmetic.py
+++ Tools/PythonTools/arithmetic.py
@@ -3,6 +3,23 @@
 import SimpleITK as sitk
 import sys
 
+
+def image_add(input_image1, input_image2):
+    return sitk.Add(input_image1, input_image2)
+
+
+def image_sub(input_image1, input_image2):
+    return sitk.Subtract(input_image1, input_image2)
+
+
+def image_div(input_image1, input_image2):
+    return sitk.Divide(input_image1, input_image2)
+
+
+def image_mul(input_image1, input_image2):
+    return sitk.Multiply(input_image1, input_image2)
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 5:
         print(sys.argv[0] + " [add|sub|div|mul] input1 input2 output")
@@ -12,21 +29,19 @@ if __name__ == "__main__":
         inputFilename2 = sys.argv[3]
         outputFilename = sys.argv[4]
 
-        if operator == "add":
-            opFilter = sitk.AddImageFilter()
-        elif operator == "sub":
-            opFilter = sitk.SubtractImageFilter()
-        elif operator == "div":
-            opFilter = sitk.DivideImageFilter()
-        elif operator == "mul":
-            opFilter = sitk.MultiplyImageFilter()
-        else:
-            print("Not supporting operator " + sys.argv[1])
-            exit(-1)
-
         inputImage1 = sitk.ReadImage(inputFilename1)
         inputImage2 = sitk.ReadImage(inputFilename2)
 
-        outputImage = opFilter.Execute(inputImage1, inputImage2)
+        if operator == "add":
+            outputImage = image_add(inputImage1, inputImage2)
+        elif operator == "sub":
+            outputImage = image_sub(inputImage1, inputImage2)
+        elif operator == "div":
+            outputImage = image_div(inputImage1, inputImage2)
+        elif operator == "mul":
+            outputImage = image_mul(inputImage1, inputImage2)
+        else:
+            print("Not supporting operator " + sys.argv[1])
+            exit(-1)
 
         sitk.WriteImage(outputImage, outputFilename)
--- Tools/PythonTools/boolean.py
+++ Tools/PythonTools/boolean.py
@@ -3,6 +3,23 @@
 import SimpleITK as sitk
 import sys
 
+
+def image_and(input_image1, input_image2):
+    return sitk.And(input_image1, input_image2)
+
+
+def image_or(input_image1, input_image2):
+    return sitk.Or(input_image1, input_image2)
+
+
+def image_xor(input_image1, input_image2):
+    return sitk.Xor(input_image1, input_image2)
+
+
+def image_not(input_image):
+    return sitk.Not(input_image)
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 5:
         print(sys.argv[0] + " [and|or|xor] input1 input2 output \n" +
@@ -13,24 +30,23 @@ if __name__ == "__main__":
         inputFilename2 = sys.argv[3]
         outputFilename = sys.argv[4]
 
+        inputImage1 = sitk.ReadImage(inputFilename1, sitk.sitkUInt16)
+        if operator != "not":
+            inputImage2 = sitk.ReadImage(inputFilename2, sitk.sitkUInt16)
+        else:
+            outputFilename = inputFilename2
+
         if operator == "and":
-            opFilter = sitk.AndImageFilter()
+            outputImage = image_and(inputImage1, inputImage2)
         elif operator == "or":
-            opFilter = sitk.OrImageFilter()
+            outputImage = image_or(inputImage1, inputImage2)
         elif operator == "xor":
-            opFilter = sitk.XorImageFilter()
-        elif operator == "not": # unary operator
-            opFilter = sitk.NotImageFilter()
+            outputImage = image_xor(inputImage1, inputImage2)
+        elif operator == "not":
+            outputImage = image_not(inputImage1)
         else:
             print("Not supporting operator " + sys.argv[1])
             exit(-1)
 
-        inputImage1 = sitk.ReadImage(inputFilename1, sitk.sitkUInt16)
-        if operator != "not":
-            inputImage2 = sitk.ReadImage(inputFilename2, sitk.sitkUInt16)
-            outputImage = opFilter.Execute(inputImage1, inputImage2)
-        else:
-            outputFilename = inputFilename2
-            outputImage = opFilter.Execute(inputImage1)
-
         sitk.WriteImage(outputImage, outputFilename)
+
