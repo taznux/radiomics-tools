c9b192275b90f371450bdf82e650dd3c5a88c606
Wookjin Choi <choiw@mskcc.org>
2016-09-10 00:02:06 -0400

    BUG: fix module installation

--- setup.py
+++ setup.py
@@ -1,8 +1,36 @@
 from setuptools import setup
+import os
 
-setup(name='qradiomcis',
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == "":
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+package_dir='qradiomics'
+
+packages = []
+for dirpath, dirnames, filenames in os.walk(package_dir):
+    # ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith("."):
+            del dirnames[i]
+    if "__init__.py" in filenames:
+        packages.append(".".join(fullsplit(dirpath)))
+
+#print(packages)
+
+setup(name='qradiomics',
         version='0.1',
-        packages=['qradiomics'],
+        packages=packages,
         author='Wookjin Choi',
         install_requires=[
             'setuptools',
